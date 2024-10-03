6d0f77e850cb8de8af1e967f11ac584900268d83
beren33 <nozeles@gmail.com>
2024-10-03 18:08:00 +0300

    Update main.py
    homework

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
-import random
-import time
+meme_dict = {
+    "LOL": "Komik bir şeye verilen cevap",
+    "CRINGE": "Garip ya da utandırıcı bir şey",
+    "ROFL": "Bir şakaya karşılık cevap",
+    "SHEESH": "Onaylamamak",
+    "CREEPY": "Korkunç",
+    "AGGRO": "Sinirlenmek",
+    "Prefer": "Tercih etmek",
+    "TEMPLE": "Tapınak",
+    "MAZE RUNNER": "ÖLÜMCÜL KAÇIŞ",
+    "CREATURE": "Yaratık",
+    "TREASURE": "Hazine",
+}
 
-
-eng_words = ['Hi','Bye','Task', 'Programm']
-tr_words = ['Merhaba','Hoşça kalın','Görev', 'Program']
-score = 0
-
-mode = input("Bir mod seçin: Yeni kelimeler eklemek için 0, çeviri yapmak için 1: \n")
-while ((mode != '0') and (mode != '1')):
-    mode = input("Geçersiz sembol! Sadece 0 veya 1 yazın (Yeni kelimeler eklemek için 0, çeviri yapmak için 1) \n")
-
-if mode == "1":
-    print("Çevirebildiğiniz kadar kelime çevirin! 10 hakkınız var!")
-    for i in range(10):
-        number = random.randint(0, len(eng_words)-1)
-        print("Tercümesi bu şekilde olmalı: " + eng_words[number])
-        if input() == tr_words[number]:
-            print("Harika!!!")
-            score += 1
-        else:
-            print("Bir yanlışlık var... Doğru kelime - " + eng_words[number])
-else:
-    word = input("İngilizce bir kelime yazın: ")
-    translate = input("Kelimenin tercümesini yazın: ")
-    if len(word) > 0 and len(translate) > 0:
-        eng_words.append(word)
-        tr_words.append(translate)
-        print("Kelime başarıyla eklendi!")
+while True:
+    words = list(meme_dict.keys())[:12] 
+    print("Aşağıdaki kelimelerden birinin anlamını isteyin:")
+    for word in words:
+        print("-", word)
+    
+    user_input = input("Bir kelime yazın veya çıkmak için 'exit' yazın: ").strip().upper()
+    
+    if user_input == 'EXIT':
+        print("Programdan çıkılıyor")
+        break
+    elif user_input in meme_dict:
+        print(meme_dict[user_input])
+    else:
+        print(" Lütfen listedeki kelimelerden birini girin.")
